6ed20e66c70d0fd051467515fcbe67c489aa0694
alanseong <7521207+alanseong@users.noreply.github.com>
2023-06-23 16:49:36 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="tomocard.png" className="App-logo" alt="logo" />
+        <br/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.tomocredit.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TomoCredit
         </a>
       </header>
     </div>
